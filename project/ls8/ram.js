1d0be2789c001a02e34514e2c69e6e927ee30da8
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-05-14 18:51:28 -0400

    Tiffany Robbins

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -13,6 +13,7 @@ class RAM {
     write(MAR, MDR) {
         // !!! IMPLEMENT ME
         // write the value in the MDR to the address MAR
+        this.mem[MAR]=MDR;
     }
 
     /**
@@ -23,7 +24,11 @@ class RAM {
     read(MAR) {
         // !!! IMPLEMENT ME
         // Read the value in address MAR and return it
+    return this.mem[MAR];
     }
 }
+
+
+// to test node ./project/ls8/ls8.js
 
 module.exports = RAM;
